e38c16aefcc44b46a877731496a24f03fad8d9e3
Tabish1511 <khaqantabish@gmail.com>
2024-07-10 20:19:17 +0200

    experiment with be(local - basic websockets) and fe(deployed on vercel with basic websocket) code

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,96 @@
-"use client";
+"use client"
+import { useEffect, useState } from 'react'
 
-import { useMemo } from 'react';
-import io from 'socket.io-client';
+export default function() {
+  const [socket, setSocket] = useState<WebSocket | null>(null);
 
-const Home = () => {
+  useEffect(() => {
+    const newSocket = new WebSocket('ws://localhost:8080');
+    newSocket.onopen = () => {
+      console.log('Connection established');
+      newSocket.send('Hello Server!');
+    }
+    newSocket.onmessage = (message) => {
+      console.log('Message received:', message.data);
+    }
+    setSocket(newSocket);
+    return () => newSocket.close();
+  }, [])
 
-  const socket = useMemo(() => io("https://journalink.onrender.com", {
-    withCredentials: true
-  }), []);
-
-
-  socket.on("connect", () => {
-    console.log("I am connected")
-  })
-
-  socket.on("message", (data, id)=> {
-    console.log(data + "||" + id)
-  });
-  
   return (
-    <div>
-      Hello there!
-    </div>
-  );
-};
+    <>
+      hi there
+    </>
+  )
+}
 
-export default Home;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// "use client";
+
+// import { useMemo } from 'react';
+// import io from 'socket.io-client';
+
+// const Home = () => {
+
+//   const socket = useMemo(() => io("https://journalink.onrender.com", {
+//     withCredentials: true
+//   }), []);
+
+
+//   socket.on("connect", () => {
+//     console.log("I am connected")
+//   })
+
+//   socket.on("message", (data, id)=> {
+//     console.log(data + "||" + id)
+//   });
+  
+//   return (
+//     <div>
+//       Hello there!
+//     </div>
+//   );
+// };
+
+// export default Home;
